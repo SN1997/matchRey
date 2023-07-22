99ed6dc20062d4bfd3bc7ebe3f92807ad7f94051
ヨーグルト <yoguruto_@outlook.com>
2023-07-22 15:34:56 +0800

    add

--- src/main/java/com/rey/matchrey/dto/MatchDataDTO.java
+++ src/main/java/com/rey/matchrey/dto/MatchDataDTO.java
@@ -6,4 +6,131 @@ package com.rey.matchrey.dto;
  * @description
  */
 public class MatchDataDTO {
+
+    private String league_name;
+
+    private String league_logo;
+
+    private String round;
+
+    private String game_time;
+
+    public aTeam left_team;
+
+    public bTeam right_team;
+
+    public static class aTeam{
+        private String id;
+
+        private String name;
+
+        private String logo;
+
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getLogo() {
+            return logo;
+        }
+
+        public void setLogo(String logo) {
+            this.logo = logo;
+        }
+    }
+
+    public static class bTeam{
+        private String id;
+
+        private String name;
+
+        private String logo;
+
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getLogo() {
+            return logo;
+        }
+
+        public void setLogo(String logo) {
+            this.logo = logo;
+        }
+    }
+
+
+    public String getRound() {
+        return round;
+    }
+
+    public void setRound(String round) {
+        this.round = round;
+    }
+
+    public aTeam getLeft_team() {
+        return left_team;
+    }
+
+    public void setLeft_team(aTeam left_team) {
+        this.left_team = left_team;
+    }
+
+    public bTeam getRight_team() {
+        return right_team;
+    }
+
+    public void setRight_team(bTeam right_team) {
+        this.right_team = right_team;
+    }
+
+    public String getLeague_name() {
+        return league_name;
+    }
+
+    public void setLeague_name(String league_name) {
+        this.league_name = league_name;
+    }
+
+    public String getLeague_logo() {
+        return league_logo;
+    }
+
+    public void setLeague_logo(String league_logo) {
+        this.league_logo = league_logo;
+    }
+
+    public String getGame_time() {
+        return game_time;
+    }
+
+    public void setGame_time(String game_time) {
+        this.game_time = game_time;
+    }
 }
--- src/main/java/com/rey/matchrey/service/MatchService.java
+++ src/main/java/com/rey/matchrey/service/MatchService.java
@@ -1,9 +1,13 @@
 package com.rey.matchrey.service;
 
+import cn.hutool.core.util.ArrayUtil;
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONUtil;
+import com.rey.matchrey.dto.MatchDataDTO;
+import mao.utils.MarkdownUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -18,9 +22,9 @@ import java.util.Map;
 @Service
 public class MatchService {
 
-    String DOTA2 ="dota";
-    String LOL = "lol";
-    String CSGO = "csgo";
+    private static String DOTA2 ="dota";
+    private static String LOL = "lol";
+    private static String CSGO = "csgo";
 
 
     /**
@@ -30,15 +34,63 @@ public class MatchService {
      * @date 2023/07/22
      */
     public void getDotaMatch() throws Exception {
-        //https://api.vpgame.com/schedule/schedule/unstart_schedule?gameIp=dota&maxGameTime=1690085458819000&minGameTime=1689999073792000
-        String data= HttpUtil.get("https://api.vpgame.com/schedule/schedule/unstart_schedule?gameIp="+LOL
+
+        String data= HttpUtil.get("https://api.vpgame.com/schedule/schedule/unstart_schedule?gameIp="+DOTA2
                                     +"&maxGameTime="+ (DateUtil.tomorrow().getTime()/1000)+"000"
                                     +"&minGameTime="+ (new Date().getTime()/1000)+"000");
-        //RespDto respDto = JSONObject.parseObject(response, RespDto.class);
+        //赛事名称
+        String leagueName = null;
+        //赛事logo
+        String leagueLogo = null;
+        //赛事MD格式
+        String leagueMd;
+        Object[] append = null;
+        Object[][] objs = new Object[0][];
 
         List<Object> dataList = (List<Object>) JSONUtil.parseObj(JSONUtil.parseObj(data).get("data")).get("all_list");
-        System.out.println(dataList);
+        for (Object obj : dataList){
+            MatchDataDTO matchData = JSONObject.parseObject(JSON.toJSONString(obj), MatchDataDTO.class);
+            String dateTime = matchData.getGame_time().substring(0,matchData.getGame_time().length()-3);
+            MatchDataDTO.aTeam aTeam = matchData.getLeft_team();
+            String aLog = this.teamLogo(aTeam.getLogo(),aTeam.getName());
+            MatchDataDTO.bTeam bTeam = matchData.getRight_team();
+            String bLog = this.teamLogo(bTeam.getLogo(),bTeam.getName());
+            leagueName = matchData.getLeague_name();
+            leagueLogo = matchData.getLeague_logo();
+            Object[] objects = new Object[]{ DateUtil.date(Long.parseLong(dateTime)*1000),aLog+aTeam.getName(), bLog+bTeam.getName(), "BO"+matchData.getRound()};
+            //TODO 数据添加问题
+            append=  ArrayUtil.append(objs, objects);
+        }
+        leagueMd = this.logo(leagueLogo,leagueName);
+        String s = this.MDW(leagueMd,leagueName, new Object[][]{append});
+        System.out.println(s);
+
     }
+
+
+    /**
+     * 获取战队图标
+     * @param url
+     * @param name
+     * @return
+     */
+    public String logo(String url,String name){
+        return MarkdownUtils.of().photo(name,url).build();
+    }
+
+    public String teamLogo(String name ,String url){
+        String s = "<img src=\""+name+"\" alt=\""+url+"\" style=\"zoom:15%;\" />";
+        return s;
+    }
+
+
+    public String MDW(String leagueMd,String leagueName,Object[][] datas){
+        String[] name = new String[]{"比赛时间", "队伍名称", "队伍名称", "BO"};
+        String s = MarkdownUtils.of(). bigTitle(leagueMd+""+leagueName)
+                .table().data(name,datas).endTable().build();
+        return s;
+    }
+
 
     //[{"id":110959,"colour":"#A0A0A0","round":3,"category":"csgo","followed":false,"status":"UNSTART","league_id":3650,"league_name":"2023年BLAST Premier:秋季总决赛","league_logo":"https://image.bnedata.com/esporst/20230702/64a102f6e9873.png","league_clickable":true,"match_index":3,"game_time":1690030800000,"bp_ongoing":false,"match_status":"UNKNOWN","left_team":{"id":35136,"name":"EG","logo":"https://image.bnedata.com/data/upload/20200523/5ec89f2a78f24.png","big_score":0,"gold_diff":0},"right_team":{"id":35135,"name":"G2","logo":"https://image.bnedata.com/data/upload/20210425/608515d16e85c.png","big_score":0,"gold_diff":0},"view_count":2,"live_video":false,"live_animation":true}]
 }
